e1f6a8aa55f3f06b9adc30dd1b5de723d2221c4c
computer <computer@192.168.0.7>
2013-09-12 15:11:39 +0900

    .

---
+++ 123/src/net/bitacademy/java41/services/MemberService.java
@@ -0,0 +1,36 @@
+package net.bitacademy.java41.services;
+
+import java.util.List;
+
+import net.bitacademy.java41.dao.MemberDao;
+import net.bitacademy.java41.vo.Member;
+
+public class MemberService {
+	MemberDao memberDao;
+	
+	public MemberService setMemberDao(MemberDao memberDao) {
+		this.memberDao = memberDao;
+		return this;
+	}
+	
+	public void signUp(Member member) throws Exception {
+		memberDao.add(member);
+	}
+
+	public List<Member> list() throws Exception{
+		return memberDao.list();
+	}
+	
+	public Member get(String email) throws Exception{
+		return memberDao.get(email);
+	}
+	
+	public int changePassword(String email, String oldPassword, String newPassword) throws Exception{
+		return memberDao.changePassword(email, oldPassword, newPassword);
+		
+	}
+	
+	public void add(Member member) throws Exception{
+	memberDao.add(member);
+	}
+}
